4342fa282bb3f2f10090324dfba639fd84f22dab
Vishal Kumar <90318739+Vishalkumar169@users.noreply.github.com>
2024-01-17 19:32:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var istatus = document.querySelector("h5")
+var btn = document.querySelector("#add")
+
+var flag = 0
+
+btn.addEventListener("click", function () {
+    if (flag == 0) {
+        istatus.innerHTML = "Freinds"
+        istatus.style.color = "green"
+        btn.innerHTML = "Remove Friend"
+        flag = 1
+    }
+    else {
+        istatus.innerHTML = "Stranger"
+        istatus.style.color = "red"
+        btn.innerHTML = "Add Friend"
+        flag = 0
+    }
+})
+
+
+
+
